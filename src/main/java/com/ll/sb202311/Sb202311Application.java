eb3ad505bc1358e1107c35024897085f14232a39
tallguy188 <cksghd4220@naver.com>
2023-11-17 15:37:26 +0900

    setting

---
+++ src/main/java/com/ll/sb202311/Sb202311Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb202311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb202311Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb202311Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb202311/Sb202311ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb202311;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb202311ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
