70bcb276fcc452c56d99d28d360c9ae1bef02855
tallguy188 <cksghd4220@naver.com>
2023-11-24 13:48:19 +0900

    add more exam

--- src/main/java/com/ll/sb202311/HomeController.java
+++ src/main/java/com/ll/sb202311/HomeController.java
@@ -1,10 +1,16 @@
 package com.ll.sb202311;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -61,4 +67,177 @@ public class HomeController {
     }
 
 
+
+    @GetMapping("/calc7")
+    @ResponseBody
+    boolean showCalc7(
+            int a, int b
+    ) {
+        return a > b;
+    }
+
+
+    @GetMapping("/calc8")
+
+    @ResponseBody
+
+    Person showCalc8(
+
+            String name, int age
+
+    ) {
+
+        return new Person(name, age);
+
+    }
+
+
+
+
+    @GetMapping("/calc9")
+
+    @ResponseBody
+
+    Person2 showCalc9(
+
+            String name, int age
+
+    ) {
+
+        return new Person2(name, age);
+
+    }
+
+
+
+
+    @GetMapping("/calc10")
+
+    @ResponseBody
+    Map<String, Object> showCalc10(
+
+            String name, int age
+
+    ) {
+
+        Map<String, Object> personMap = Map.of(
+
+                "name", name,
+
+                "age", age
+
+        );
+
+
+
+
+        return personMap;
+
+    }
+
+
+
+
+    @GetMapping("/calc11")
+
+    @ResponseBody
+    List<Integer> showCalc11() {
+
+        List<Integer> nums = new ArrayList<>() {{
+
+            add(10);
+
+            add(-510);
+
+            add(10010);
+
+        }};
+
+
+
+
+        return nums;
+
+    }
+
+
+
+
+    @GetMapping("/calc12")
+
+    @ResponseBody
+
+    int[] showCalc12() {
+
+        int[] nums = new int[]{10, -510, 10010};
+
+
+
+
+        return nums;
+
+    }
+
+
+
+
+    @GetMapping("/calc13")
+
+    @ResponseBody
+
+    List<Person2> showCalc13(
+
+            String name, int age
+
+    ) {
+
+        List<Person2> persons = new ArrayList<>() {{
+
+            add(new Person2(name, age));
+
+            add(new Person2(name + "!", age + 1));
+
+            add(new Person2(name + "!!", age + 2));
+
+        }};
+
+
+
+
+        return persons;
+
+    }
+
 }
+
+
+
+
+@AllArgsConstructor
+
+class Person {
+
+    public String name;
+
+    public int age;
+
+}
+
+
+
+
+@AllArgsConstructor
+
+class Person2 {
+
+    @Getter
+
+    private String name;
+
+    @Getter
+
+    private int age;
+
+}
+
+
