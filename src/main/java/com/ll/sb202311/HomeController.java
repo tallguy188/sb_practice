57435a79e12113cd379c089fc5796176897369dc
tallguy188 <cksghd4220@naver.com>
2023-11-24 14:29:50 +0900

    before MVC

--- src/main/java/com/ll/sb202311/HomeController.java
+++ src/main/java/com/ll/sb202311/HomeController.java
@@ -81,7 +81,7 @@ public class HomeController {
 
     @ResponseBody
 
-    Person showCalc8(
+    Person showCalc8(          // return이 그대로 렌더링되는 것이 아니라 springboot가 자체해석해서 json값으로 렌더링해준다는 것을 배움
 
             String name, int age
 
@@ -206,6 +206,83 @@ public class HomeController {
 
         return persons;
 
+    }
+    @GetMapping("/calc14")
+    @ResponseBody
+    String showCalc14() {
+        String html = "";
+
+        html += "<div>";
+        html += "<input type=\"text\" placeholder=\"내용\">";
+        html += "</div>";
+
+        return html;
+    }
+
+    @GetMapping("/calc15")
+    @ResponseBody
+    String showCalc15() {
+
+        // 다수의 문자열을 이어붙일때는 StringBuilder를 사용
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<div>");
+        sb.append("<input type=\"text\" placeholder=\"내용\">");
+        sb.append("</div>");
+
+        return sb.toString();
+    }
+
+    @GetMapping("/calc16")
+    @ResponseBody
+    String showCalc16() {
+        String html = "<div><input type=\"text\" placeholder=\"내용\"></div>";
+
+        return html;
+    }
+
+    @GetMapping("/calc17")
+    @ResponseBody
+    String showCalc17() {
+        String html = """
+                <div>
+                    <input type="text" placeholder="내용">
+                </div>
+                """;
+
+        return html;
+    }
+
+    @GetMapping("/calc18")// calc 14 ~ 18의 결과는 같음
+    @ResponseBody
+    String showCalc18() {
+        // 이스케이프시퀀스를 쓰고싶지 않으면 """를 써라.
+        String html = """
+                <div>
+                    <input type="text" placeholder="내용" value="반가워요.">
+                </div>
+                """;
+
+        return html;
+    }
+
+    @GetMapping("/calc19")
+    @ResponseBody
+    String showCalc19(  // MVC에서 view로 넘기지 않으면 이렇게 html을 길게 써야되는 상황이 된다.
+            @RequestParam(defaultValue = "") String subject,
+            @RequestParam(defaultValue = "") String content
+    ) {
+
+        String html = """        
+                <div>
+                    <input type="text" placeholder="제목" value="%s">
+                </div>
+                <div>
+                    <input type="text" placeholder="내용" value="%s">
+                </div>
+                """.formatted(subject, content);
+
+        return html;
     }
 
 }
