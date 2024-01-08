8b98eefd813f58fe8b826c0e8822ed26968b1cc9
tallguy188 <cksghd4220@naver.com>
2024-01-08 09:50:50 +0900

    step 55, use writeForm, valid

--- src/main/java/com/ll/sb202311/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb202311/domain/article/article/controller/ArticleController.java
@@ -4,10 +4,12 @@ import com.ll.sb202311.domain.article.article.entity.Article;
 import com.ll.sb202311.domain.article.article.service.ArticleService;
 import com.ll.sb202311.global.rq.Rq;
 import com.ll.sb202311.global.rsData.RsData.RsData;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,7 +18,6 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@Validated
 public class ArticleController {
 
     private final ArticleService articleService;
@@ -27,16 +28,22 @@ public class ArticleController {
         return "article/write";
     }
 
+
+    @Getter
+    @Setter
+    public static class WriteForm {
+        @NotBlank
+        private String title;
+        @NotBlank
+        private String body;
+
+    }
     @PostMapping("/article/write")
     @ResponseBody
-    RsData doWrite(
-            @NotBlank
-            String title,
-            @NotBlank
-            String body
+    RsData doWrite(@Valid WriteForm writeForm
     ) {
 
-        Article article =  articleService.write(title,body);
+        Article article =  articleService.write(writeForm.title,writeForm.body);
 
         RsData<Article> rs = new RsData<>(  // article버전 rsData
                 "S-1",
